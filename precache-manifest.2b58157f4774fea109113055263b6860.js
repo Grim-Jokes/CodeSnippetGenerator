1b71102a3297755d9cdb7c909a8d78bf0f2f64cb
Daniel Szekely <danszekely@gmail.com>
2020-05-25 17:51:06 -0400

    Updates

---
+++ precache-manifest.2b58157f4774fea109113055263b6860.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cac27441209fee7b2c8286e3ba44169",
+    "url": "/index.html"
+  },
+  {
+    "revision": "700db7e3015a67821fdd",
+    "url": "/static/css/main.0b7423ea.chunk.css"
+  },
+  {
+    "revision": "91ec1ad1768ddc215aeb",
+    "url": "/static/js/2.ab53242b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab53242b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "700db7e3015a67821fdd",
+    "url": "/static/js/main.85b8d57e.chunk.js"
+  },
+  {
+    "revision": "f6fbed95c7b455b4dd0d",
+    "url": "/static/js/runtime-main.e584c015.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
